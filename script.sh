b33f9ccd224651a38d137326102353a81a73335c
Brian Harring <ferringb@gmail.com>
2014-02-21 14:09:35 +0000

    Fix find invocation so that symlinks can be used

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ mkdir git -p
 # the most revs, thus start them first.
 f() {
   time { \
-    find cvs-repo/gentoo-x86 -maxdepth 1 -mindepth 1 -printf '%P\n' | \
+    find cvs-repo/gentoo-x86/ -maxdepth 1 -mindepth 1 -printf '%P\n' | \
     xargs -n1 -I{} --  du -cs "cvs-repo/gentoo-x86/{}" | grep -v 'total$' | \
     sort -gr | awk '{print $2;}' | xargs -n1 basename | \
     xargs -n1 -P${proc_count} -I{} ./process_directory.sh "{}" $1
