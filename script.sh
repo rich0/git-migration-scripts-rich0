830e12ee2c88e586d24fad14470ae08a1071dd5f
Richard Freeman <rich0@gentoo.org>
2014-09-29 21:33:22 -0400

    Process profiles first.
    Since each category is single-threaded, starting the longest one first
    minimizes idle cpu time.

--- script.sh
+++ script.sh
@@ -10,10 +10,12 @@ mkdir git -p
 # the most revs, thus start them first.
 f() {
   time { \
-    find cvs-repo/gentoo-x86/ -maxdepth 1 -mindepth 1 -printf '%P\n' | \
-    xargs -n1 -I{} --  du -cs "cvs-repo/gentoo-x86/{}" | grep -v 'total$' | \
-    sort -gr | awk '{print $2;}' | xargs -n1 basename | \
-    xargs -n1 -P${proc_count} -I{} ./process_directory.sh "{}" $1
+    ( \
+      echo "profiles" ; \
+      find cvs-repo/gentoo-x86/ -maxdepth 1 -mindepth 1 -printf '%P\n' | \
+      xargs -n1 -I{} --  du -cs "cvs-repo/gentoo-x86/{}" | grep -v 'total$' | \
+      sort -gr | awk '{print $2;}' | xargs -n1 basename | grep -v profiles \
+    ) | xargs -n1 -P${proc_count} -I{} ./process_directory.sh "{}" $1
   }
 }
 fast=false
